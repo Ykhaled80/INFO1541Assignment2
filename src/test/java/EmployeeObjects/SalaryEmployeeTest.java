4c5d222d5a7c0050b40750821be2435d8a9687a3
Ykhaled80 <153566601+Ykhaled80@users.noreply.github.com>
2024-03-15 23:37:07 -0500

    SalaryEmployeeTest class both of the unit tests are corrected and Finished.

--- src/test/java/EmployeeObjects/SalaryEmployeeTest.java
+++ src/test/java/EmployeeObjects/SalaryEmployeeTest.java
@@ -9,7 +9,7 @@ class SalaryEmployeeTest {
     @Test
     void calculateWeeklyPay() {
         SalaryEmployee emp = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
-        double weeklyExpectedPay = Math.round(64325.0 / 52 * 100.) / 100.0;
+        double weeklyExpectedPay = Math.round(emp.calculateWeeklyPay() * 100.) / 100.0;
         assertEquals(1237.02, weeklyExpectedPay);
 
     }
@@ -17,7 +17,6 @@ class SalaryEmployeeTest {
     @Test
     void holidayBonus() {
         SalaryEmployee emp = new SalaryEmployee("Steve", "Rodgers", 3781, "Sales", "Manager", 64325);
-        double bonus = Math.round(64325.0 * 0.03365 * 100.0) / 100.0;
         double actualBonus = Math.round(emp.holidayBonus() * 100.0) / 100.0;
         assertEquals(2164.54, actualBonus);
     }
